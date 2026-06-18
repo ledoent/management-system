2f9e946118f7fcc3c8cb6c67ed36064d609efe9d
Don Kendall <dkendall@ledoweb.com>
2026-06-18 12:15:12 -0400

    [MIG] mgmtsystem_nonconformity_maintenance_equipment: Migration to 19.0

--- mgmtsystem_nonconformity_maintenance_equipment/__manifest__.py
+++ mgmtsystem_nonconformity_maintenance_equipment/__manifest__.py
@@ -3,7 +3,7 @@
 
 {
     "name": "Management System - Nonconformity Maintenance Equipment",
-    "version": "18.0.1.0.0",
+    "version": "19.0.1.0.0",
     "author": "Escodoo, Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/management-system",
     "license": "AGPL-3",
